356534a178765b19ad9403c6cf0e8342d4fd0b41
Egill Hreinson <egillh210@gmail.com>
2019-09-03 15:37:41 +0200

    feat: timing logic working

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ let initialState = {
   currentTime: 0,
 
   currentLapTime: 0,
-  previousLapTime: null,
+  previousLapTime: 0,
 
   slowestLapTimeId: null,
   fastestLapTimeId: null,
@@ -90,6 +90,20 @@ const timer = (state = initialState, action) => {
         currentLapTime,
       })
     }
+    case LAP: {
+      const { currentTime, laps, previousLapTime } = state;
+      const newLap = {
+        id: nextId,
+        lapTime: currentTime - previousLapTime,
+      };
+      nextId++;
+      return ({
+        ...state,
+        currentLapTime: 0,
+        previousLapTime: currentTime,
+        laps: [...laps].concat(newLap),
+      })
+    }
     default:
       return state;
   }
@@ -109,6 +123,40 @@ function showTime ({ currentTime: time } = 0) {
   timeDisplay.innerHTML = `${hours} : ${minutes} : ${seconds} : ${milliseconds}`;
 }
 
+function getTimeAsAString (time) {
+  let hours = Math.floor(time % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
+  let minutes = Math.floor(time % (1000 * 60 * 60) / (1000 * 60));
+  let seconds = Math.floor(time % (1000 * 60) / (1000));
+  let milliseconds = Math.floor(time % (1000) / 10);
+
+  hours = (hours < 10) ? '0' + hours : hours;
+  minutes = (minutes < 10) ? '0' + minutes : minutes;
+  seconds = (seconds < 10) ? '0' + seconds : seconds;
+  milliseconds = (milliseconds < 10) ? '0' + milliseconds : milliseconds;
+
+  return `${hours} : ${minutes} : ${seconds} : ${milliseconds}`;
+}
+
+function showLaps ({ laps }) {
+  let arrLapsStr = laps.map(obj => {
+    const { id, lapTime } = obj;
+    console.log(lapTime);
+    let newLapNode = document.createElement('li');
+    const newLap = document.createTextNode(`Lap ${id} : ${getTimeAsAString(lapTime)}`);
+    newLapNode.append(newLap);
+   //const newLapNode = `Lap ${id} : ${getTimeAsAString(lapTime)}`;
+    return newLapNode;
+  })
+  arrLapsStr = arrLapsStr.reverse();
+  // document.querySelector('.lapsContainer').innerHTML = '';
+  // document.querySelector('.lapsContainer').append(arrLapsStr);
+  var container = document.querySelector('.lapsContainer');
+  container.innerHTML = '';
+  arrLapsStr.forEach(t => {
+    container.appendChild(t);
+  })
+}
+
 var btn = document.querySelectorAll('button');
 var hours, minutes, seconds, time, state, tInterval;
 var timeDisplay = document.querySelector('.timeDisplay');
@@ -116,31 +164,30 @@ var timeDisplay = document.querySelector('.timeDisplay');
 document.querySelector('.timerContainer').addEventListener('click', e => {
   let time = Date.now();
   state = timer(state, getCurrentTime(time));
-  console.log('here', state);
+  //console.log('here', state);
 },true);
 
 document.addEventListener('click', function (event) {
   event.preventDefault();
 
-  if(event.target.matches('.timerContainer')) {
-    //console.log('here!');
-    // let time = Date.now();
-    // state = timer(state, getCurrentTime(time));
-  }
-
   if(event.target.matches('.startTimer')) {
     let time = Date.now();
     state = timer(state, startTimer(time))
-    console.log(state);
+    //console.log(state);
   }
   if(event.target.matches('.stopTimer')) {
     let time = Date.now();
     state = timer(state, stopTimer(time))
-    console.log(state);
+    //console.log(state);
   }
   if(event.target.matches('.resetTimer')) {
     let time = Date.now();
     state = timer(state, resetTimer());
-    console.log(state);
+    //console.log(state);
+  }
+  if(event.target.matches('.lap')) {
+    state = timer(state, lapTimer());
+    showLaps(state);
+    //console.log(state);
   }
 });
